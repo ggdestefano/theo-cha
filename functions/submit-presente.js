ff7d2ce2b64455cf710f2495e483bd5b351ccca2
ggdestefano <gustavogestech@gmail.com>
2025-10-17 13:07:34 -0300

    Update submit-presente.js

--- functions/submit-presente.js
+++ functions/submit-presente.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 exports.handler = async (event, context) => {
     if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Método só POST' };
 
-    const { presente } = JSON.parse(event.body);
+    const { presente, nome } = JSON.parse(event.body);
     const FORM_ID = 'lKoCJSy5rO0';
     const API_KEY = 'k100jb17xHTsuj9aCpmfUS4MYOTJTGGX2o1EXDiG80VYnwVJBYjLGKD9tfOHIiuz';
 
@@ -11,7 +11,12 @@ exports.handler = async (event, context) => {
         const response = await fetch(`https://formcarry.com/api/form/${FORM_ID}/submissions`, {
             method: 'POST',
             headers: { 'api_key': API_KEY, 'Content-Type': 'application/json' },
-            body: JSON.stringify({ fields: [{ name: 'presente', value: presente }] })
+            body: JSON.stringify({
+                fields: [
+                    { name: 'presente', value: presente },
+                    { name: 'nome', value: nome }
+                ]
+            })
         });
 
         if (!response.ok) throw new Error(`Erro: ${response.status}`);
